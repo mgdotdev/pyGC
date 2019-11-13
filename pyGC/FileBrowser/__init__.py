1b8fa669abc24b92154d52679020f8555493cc0a
1mikegrn <1mikegrn@gmail.com>
2019-11-13 14:48:27 -0600

    cleaning clutter

--- pyGC/FileBrowser/__init__.py
+++ pyGC/FileBrowser/__init__.py
@@ -493,28 +493,3 @@ class FileBrowser(BoxLayout):
     def _attr_callback(self, attr, obj, value):
         setattr(self, attr, getattr(obj, attr))
 
-if __name__ == '__main__':
-    import os
-    from kivy.app import App
-
-    class TestApp(App):
-
-        def build(self):
-            user_path = os.path.join(get_home_directory(), 'Documents')
-            browser = FileBrowser(select_string='Select',
-                                  favorites=[(user_path, 'Documents')])
-            browser.bind(on_success=self._fbrowser_success,
-                         on_canceled=self._fbrowser_canceled,
-                         on_submit=self._fbrowser_submit)
-            return browser
-
-        def _fbrowser_canceled(self, instance):
-            print('cancelled, Close self.')
-
-        def _fbrowser_success(self, instance):
-            print(instance.selection)
-
-        def _fbrowser_submit(self, instance):
-            print(instance.selection)
-
-    TestApp().run()
